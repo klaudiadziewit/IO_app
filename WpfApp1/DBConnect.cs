707e3bfc7a6aab8e9b83e36ee9b6f0c3c4636789
klaudiadziewit <klaudia.dziewit@gmail.com>
2018-01-22 11:51:52 +0100

    Deleted comments

--- WpfApp1/DBConnect.cs
+++ WpfApp1/DBConnect.cs
@@ -23,14 +23,11 @@ namespace WpfApp1
             private string uid;
             private string password;
 
-            //Constructor
             public DBConnect()
             {
                 Initialize();
             }
 
-          
-            //Initialize values
             public void Initialize()
             {
                 server = "mysql.agh.edu.pl";
@@ -44,13 +41,11 @@ namespace WpfApp1
              connection = new MySqlConnection(connectionString);
             }
 
-            //open connection to database
             public bool OpenConnection()
             {
                 try
                 {
                     connection.Open();
-                    //MessageBox.Show("Poprawnie połączono");
                     return true;
                 }
                 catch (MySqlException ex)
@@ -70,13 +65,11 @@ namespace WpfApp1
                 }
             }
 
-        //Insert statement
         public void Insert()
         {
             if (this.OpenConnection() == true)
             {   
-                MySqlCommand cmd = new MySqlCommand("insert into zgloszenie_szkody_samochodowej VALUES(NULL,@data_zgloszenia,@status_zgloszenia,@Polisa_samochodowa_ID, @kraj, @miasto, @ulica, @policja, @samochod_zastepczy, @laweta, @numer_policji, @id_klienta);", connection);
-                
+                MySqlCommand cmd = new MySqlCommand("insert into zgloszenie_szkody_samochodowej VALUES(NULL,@data_zgloszenia,@status_zgloszenia,@Polisa_samochodowa_ID, @kraj, @miasto, @ulica, @policja, @samochod_zastepczy, @laweta, @numer_policji, @id_klienta);", connection);                
                 DateTime enteredDate = DateTime.Parse("22-02-2017");
                 cmd.Parameters.AddWithValue("@data_zgloszenia", enteredDate);
                 cmd.Parameters.AddWithValue("@status_zgloszenia", "blabla");
@@ -107,68 +100,39 @@ namespace WpfApp1
                 {
                     MessageBox.Show(ex.Message);
                     return false;
-                }
-                
+                }                
             }
-
 
         public List<string>[] Select()
         {
             string query = "SELECT * FROM klienci";
 
-            //Create a list to store the result
             List<string>[] list = new List<string>[3];
             list[0] = new List<string>();
             list[1] = new List<string>();
             list[2] = new List<string>();
-            int ID;
 
-            //Open connection
             if (this.OpenConnection() == true)
             {
-                //Create Command
                 MySqlCommand cmd = new MySqlCommand(query, connection);
-                //Create a data reader and Execute the command
                 MySqlDataReader dataReader = cmd.ExecuteReader();
 
-                //Read the data and store them in the list
                 while (dataReader.Read())
                 {
-                    //list[0].Add(dataReader["ID"] + "");
-                    //list[1].Add(dataReader["imie"] + "");
-                    //list[2].Add(dataReader["pesel"] + "");
-                    //ID = Convert.ToInt32(dataReader["ID"] + "");
                     MessageBox.Show(dataReader["ID"] + "");
                     MessageBox.Show(dataReader["imie"] + "");
                 }
 
-
-                //close Data Reader
                 dataReader.Close();
-
-                // for( int i=0; i<2; i++) 
-                // {
-                //Console.WriteLine(list[i]);
-                //string name = (list[i])
-                //MessageBox.Show(name);
-                // }
-                // close Connection
-                //MessageBox.Show(ID);
                 this.CloseConnection();
 
-                //return list to be displayed
                 return list;
             }
             else
             {
                 return list;
             }
-
-            //string name = Convert.ToString(list[0]);
-            //   MessageBox.Show(name);
         }
-
-
     }
 
 
